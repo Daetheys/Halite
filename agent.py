07c72ab0ee5d85ad6b23a9ef3039895a6f5738af
shirenn <shirenn@crans.org>
2020-12-07 19:00:00 +0100

    typos and word wrapping

--- agent.py
+++ agent.py
@@ -15,40 +15,39 @@ class QAgent(Agent):
         self.model = model
 
     def step(self,obs,ship,actions, ACTIONS, get_actions):
+        '''Choose a random action from a set for the ship according to the Q
+        value distribution.
+        '''
         #Obs is of shape [batch_size,*inp_shape]
-        action_size = len(ACTIONS.keys())
         (obs,config) = obs
         board = Board(obs,config)
-        actions = get_actions(player,ship,config) # Get possible shipyard actions
-        Q = np.zeros((action_size,))
-        new_obs = [None]*action_size
-        for a in actions:
+        actions = get_actions(board.current_player,ship,config) # Get possible shipyard actions
+        actions_size = len(actions)
+        Q = np.zeros((actions_size,))
+        new_obs = [None]*actions_size
+        for i,a in enumerate(actions):
             obs = simulate(board,ship,a) # Simulate move
             inps = get_input(obs,config) # Get inputs for nn
             q = self.model(inps) # Get v value
-            Q[a] = q
-            new_obs[a] = obs
+            Q[i] = q
+            new_obs[i] = obs
         Q /= np.sum(Q) # Turn Q into a probability vector and sample next action from it
-        action = np.random.choice(range(action_size),p=Q)
+        action = np.random.choice(range(actions_size),p=Q)
         board = Board(new_obs[action],config) # Update board
-        return (new_obs[action],config), ACTIONS[action]
+        return (new_obs[action],config), actions[action]
 
     def conversion_step(self,obs,ship,actions):
-        return self.step(obs,ship,actions,SHIPYARD_ACTIONS,get_shipyard_actions)
+        return self.step(obs,ship,actions,get_shipyard_actions)
 
     def conversion_step(self,obs,ship,actions)
-        return self.step(obs,ship,actions,SHIP_ACTIONS,get_ship_actions)
+        return self.step(obs,ship,actions,get_ship_actions)
 
     def get_actions(self,obs):
         actions = []
-        #Compute Q values
-        player = board.current_player
-        #1. SPAWNING
-        for ship in player.ships:
+        for ship in player.ships: #1. SPAWNING
             obs, action = self.spawning_step(obs, ship, actions)
             actions.append(action)
-        #2. Conversion and 3. Movement
-        for ship in player.ships:
+        for ship in player.ships: #2. Conversion and 3. Movement
             obs, action = self.conversion_step(obs, ship, actions)
             actions.append(action)
         return actions
--- tools.py
+++ tools.py
@@ -21,26 +21,16 @@ def shipyard_array(l):
 SHIP_ACTIONS = [ShipAction.WEST,ShipAction.NORTH,ShipAction.EAST,ShipAction.SOUTH,None,ShipAction.CONVERT]
 def get_ship_actions(p,s,config):
     #0:left 1:top 2:right 3:bottom 4:collect 5:yard
-    actions = [0,1,2,3,4,5]
-    if s.position.x == 0:
-        actions.remove(0)
-    if s.position.x == 20:
-        actions.remove(2)
-    if s.position.y == 0:
-        actions.remove(1)
-    if s.position.y == 20:
-        actions.remove(3)
     if p.halite < config["convertCost"]:
-        actions.remove(6)
-    return actions
+        return SHIP_ACTIONS[:-1]
+    return SHIP_ACTIONS
 
 SHIPYARD_ACTIONS = [None,ShipyardAction.SPAWN]
 def get_shipyard_actions(p,s,config):
     #0:pass 1:spawn
-    actions = [0,1]
-    if p.halite > config["spawnCost"]:
-        actions.remove(1)
-    return actions
+    if p.halite < config["spawnCost"]:
+        return SHIPYARD_ACTIONS[:-1]
+    return SHIPYARD_ACTIONS
 
 def get_input(obs,config):
     board = Board(obs,config)
